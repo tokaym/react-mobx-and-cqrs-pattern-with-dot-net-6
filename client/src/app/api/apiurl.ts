11b7deb37f29fb4dafcf79d9d4d91a4b856663a1
tokay <55059335+tokaym@users.noreply.github.com>
2023-05-11 09:58:42 +0300

    Update apiurl.ts

--- client/src/app/api/apiurl.ts
+++ client/src/app/api/apiurl.ts
@@ -1,5 +1,3 @@
 import axios from "axios";
 
-
-export function setaxiosURL() { axios.defaults.baseURL = "http://arank50vt/sparetestapi/api" }
-// export function setaxiosURL() { axios.defaults.baseURL = "http://localhost:5024/api" }
+export function setaxiosURL() { axios.defaults.baseURL = "http://localhost:5024/api" }
